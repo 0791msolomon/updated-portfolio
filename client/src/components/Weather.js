9319265130c9b91d0d8f5aef0a8b659968c64d00
Matt <matthew@realtimereviews.com>
2019-04-14 17:54:47 -0700

    configured .env file

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -1,7 +1,7 @@
 import React from "react";
 class Weather extends React.Component {
   componentDidMount = () => {
-    console.log(process.env.REACT_APP_SHIT);
+    console.log(1);
   };
   render() {
     return (
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const router = require("./client/app/routes");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 const port = process.env.PORT || 5000;
-
+const weatherApi = process.env.REACT_APP_WEATHER_API_KEY;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  console.log("api/hello accessed");
-  res.send({ express: "Hello From Express" });
-});
-app.post("/api/world", (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
+// app.use(function(req, res, next) {
+//   console.log(req);
+//   next();
+// });
+app.use(router);
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  console.log(weatherApi);
+});
