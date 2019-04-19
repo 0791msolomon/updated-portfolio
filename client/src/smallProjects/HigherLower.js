850f6dd250452a95d21526a214c0ca8213c9ceff
Matt <matthew@realtimereviews.com>
2019-04-18 20:27:19 -0700

    finishing higher/lower page. working on displaying alerts correctly with react-alert

--- client/src/smallProjects/HigherLower.js
+++ client/src/smallProjects/HigherLower.js
@@ -1,9 +1,29 @@
 import React from "react";
 import Input from "./HighLowInputs";
 import HigherLowerSelect from "./HigherLowerSelect";
+import { positions, Provider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import { useAlert } from "react-alert";
+
+import _ from "underscore";
 import "./index.css";
+
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER
+};
+
 class HigherLower extends React.Component {
-  state = { numbersEntered: false, low: "", high: "", errors: {} };
+  state = {
+    numbersEntered: false,
+    low: "",
+    high: "",
+    errors: {},
+    clientRandomNumber: null,
+    hiddenRandomNumber: null,
+    incorrectGuesses: 0,
+    correctGuesses: 0
+  };
   onChange = e => {
     e.preventDefault();
     this.setState({
@@ -22,23 +42,19 @@ class HigherLower extends React.Component {
         }
       });
       return;
-    }
-    if (high === "") {
+    } else if (high === "") {
       this.setState({
         numbersEntered: false,
         errors: { high: "Please enter a number" }
       });
       return;
-    }
-    if (Number(low) > Number(high)) {
-      console.log(low, high);
+    } else if (Number(low) > Number(high)) {
       this.setState({
         numbersEntered: false,
         errors: { low: "Low number cannot be above high number" }
       });
       return;
-    }
-    if (Number(high) - Number(low) < 10) {
+    } else if (Number(high) - Number(low) < 10) {
       this.setState({
         numbersEntered: false,
         errors: {
@@ -46,70 +62,106 @@ class HigherLower extends React.Component {
         }
       });
       return;
+    } else {
+      this.setState({
+        clientRandomNumber: _.random(Number(low), Number(high)),
+        hiddenRandomNumber: _.random(Number(low), Number(high))
+      });
     }
-    console.log(this.state);
     this.setState({ errors: {}, numbersEntered: true });
   };
+
+  submitAnswer = answer => {
+    const { clientRandomNumber, hiddenRandomNumber, high, low } = this.state;
+    if (
+      (clientRandomNumber > hiddenRandomNumber && answer === "higher") ||
+      (clientRandomNumber < hiddenRandomNumber && answer === "lower")
+    ) {
+      this.setState({
+        correctGuesses: (this.state.correctGuesses += 1),
+        clientRandomNumber: _.random(Number(low), Number(high)),
+        hiddenRandomNumber: _.random(Number(low), Number(high))
+      });
+    } else {
+      alert.error("wrong");
+      this.setState({
+        incorrectGuesses: (this.state.incorrectGuesses += 1),
+        clientRandomNumber: _.random(Number(low), Number(high)),
+        hiddenRandomNumber: _.random(Number(low), Number(high))
+      });
+    }
+  };
   render() {
-    const { numbersEntered, errors } = this.state;
+    const {
+      numbersEntered,
+      errors,
+      clientRandomNumber,
+      incorrectGuesses
+    } = this.state;
     return (
       <React.Fragment>
-        <div className="higherLowerContainer ">
-          <h1 class="display-1">Higher or lower?</h1>
-          <blockquote class="blockquote text-center">
-            <p class="mb-0">
-              {`Set a high number limit & a low number limit, then when a number is displayed guess if the number shown is higher or lower than the hidden number`}
-            </p>
+        <Provider template={AlertTemplate} {...options}>
+          <div className="higherLowerContainer ">
+            <h1 class="display-1">Higher or lower?</h1>
+            <blockquote class="blockquote text-center">
+              <p class="mb-0">
+                {`Set a high number limit & a low number limit, then when a number is displayed guess if the number shown is higher or lower than the hidden number`}
+              </p>
 
-            <footer class="blockquote-footer">
-              Rules Are <i className="fa fa-arrow-circle-down  " />
-              <ul style={{ listStyle: "none" }}>
-                <li>
-                  Make sure high and low are at least 10 apart or it's no fun
-                </li>
-                <li>Don't go above 500 for high number</li>
-                <li>Don't go below -500 for low number</li>
-                <li>You go until you get 10 wrong</li>
-                <li>Wrong guesses will be displayed on bottom</li>
-              </ul>
-            </footer>
-          </blockquote>
+              <footer class="blockquote-footer">
+                Rules Are <i className="fa fa-arrow-circle-down  " />
+                <ul style={{ listStyle: "none" }}>
+                  <li>
+                    Make sure high and low are at least 10 apart or it's no fun
+                  </li>
+                  <li>Don't go above 500 for high number</li>
+                  <li>Don't go below -500 for low number</li>
+                  <li>You go until you get 10 wrong</li>
+                  <li>Wrong guesses will be displayed on bottom</li>
+                </ul>
+              </footer>
+            </blockquote>
 
-          <form className="inputContainer col-6">
-            <Input
-              name="low"
-              value={this.state.low}
-              placeholder="Low Limit"
-              errors={errors.low}
-              onChange={this.onChange}
-            />
-            <Input
-              name="high"
-              value={this.state.high}
-              placeholder="High Limit"
-              errors={errors.high}
-              onChange={this.onChange}
-            />
-            <button
-              onClick={this.setLimits}
-              className=" inputNumber form-control btn-success"
-            >
-              Set Limits
-            </button>
-          </form>
-          {numbersEntered ? (
-            <div
-              style={{
-                marginTop: "3%",
-                height: "100%",
-                width: "100%",
-                textAlign: "center"
-              }}
-            >
-              <HigherLowerSelect />
-            </div>
-          ) : null}
-        </div>
+            <form className="inputContainer col-6">
+              <Input
+                name="low"
+                value={this.state.low}
+                placeholder="Low Limit"
+                errors={errors.low}
+                onChange={this.onChange}
+              />
+              <Input
+                name="high"
+                value={this.state.high}
+                placeholder="High Limit"
+                errors={errors.high}
+                onChange={this.onChange}
+              />
+              <button
+                onClick={this.setLimits}
+                className=" inputNumber form-control btn-success"
+              >
+                Set Limits
+              </button>
+            </form>
+            {numbersEntered ? (
+              <div
+                style={{
+                  marginTop: "3%",
+                  height: "100%",
+                  width: "100%",
+                  textAlign: "center"
+                }}
+              >
+                <HigherLowerSelect
+                  clientNumber={clientRandomNumber}
+                  click={this.submitAnswer}
+                  wrong={incorrectGuesses}
+                />
+              </div>
+            ) : null}
+          </div>
+        </Provider>
       </React.Fragment>
     );
   }
--- client/src/smallProjects/HigherLowerSelect.js
+++ client/src/smallProjects/HigherLowerSelect.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 const HigherLowerSelect = props => {
+  const displayErrors = () => {
+    return [...Array(props.wrong)].map((e, i) => (
+      <i
+        key={i}
+        className="col-xl-1 col-md-3 col-sm-4 col-xs-6  fa fa-close fa-3x"
+        style={{ color: "red" }}
+      />
+    ));
+  };
   return (
     <div className="col-12" style={{ marginBottom: "2%" }}>
       <div className=" highlowselectcontainer">
         <button
           className="arrowOptions col-lg-3 col-sm-12 btn-success"
           style={{ alignSelf: "center" }}
+          onClick={() => props.click("higher")}
         >
           <i className="fa fa-arrow-circle-up fa-5x" />
         </button>
@@ -14,7 +24,7 @@ const HigherLowerSelect = props => {
           className="arrowOptions col-lg-3 col-sm-12"
           style={{ display: "flex", flexDirection: "column" }}
         >
-          <h1 class="display-4">438</h1>
+          <h1 class="display-4">{props.clientNumber}</h1>
           <h4 class="display-6">
             Do you think this is Higher or Lower than the hidden number?
           </h4>
@@ -22,17 +32,13 @@ const HigherLowerSelect = props => {
         <button
           className="arrowOptions col-lg-3 col-sm-12 btn-danger"
           style={{ alignSelf: "center" }}
+          onClick={() => props.click("lower")}
         >
           <i className="fa fa-arrow-circle-down fa-5x " />
         </button>
       </div>
       <hr className="my-4" />
-      <div className="wrongAnswers col-12">
-        <i
-          className="col-xl-1 col-md-3 col-sm-4 col-xs-6  fa fa-close fa-3x"
-          style={{ color: "red" }}
-        />
-      </div>
+      <div className="wrongAnswers col-12">{displayErrors()}</div>
     </div>
   );
 };
