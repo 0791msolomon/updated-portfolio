a5ea2f3cac37935d417328f42926db6128d7be10
Matthew <0791msolomon@gmail.com>
2019-04-29 10:15:11 -0700

    added path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./app/routes");
 const app = express();
+const path = require("path");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -13,21 +14,13 @@ const url =
 const port = process.env.PORT || 5000;
 const weatherApi = process.env.REACT_APP_WEATHER_API_KEY;
 // app.use(cors);
+
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(router);
 
-// mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   app.listen(port, () => {
-//     console.log(`Listening on port ${port}, database successfully connected  `);
-//   });
-// });
-// mongoose.connect(url, { useNewUrlParser: true });
 mongoose.connect(url, { useNewUrlParser: true }, function(err) {
   if (err) {
     console.log(err);
