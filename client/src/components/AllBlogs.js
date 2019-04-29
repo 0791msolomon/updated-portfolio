afb4bdbb4ae33e17043a9413d5f4392b4bcc8ccf
Matt <matthew@realtimereviews.com>
2019-04-28 21:26:44 -0700

    progress on all blogs page

--- client/src/components/AllBlogs.js
+++ client/src/components/AllBlogs.js
@@ -17,14 +17,33 @@ class AllBlogs extends React.Component {
         console.log(err);
       });
   };
+  setDisplayBlog = blog => {
+    this.setState({
+      displayBlog: blog
+    });
+  };
 
   displayBlogs = () => {
-    return this.state.blogs.map(blog => {
+    return this.state.blogs.map((blog, i) => {
+      if (i > 4 || blog._id === this.state.displayBlog._id) {
+        return;
+      }
       return (
-        <div class="card" key={blog.id}>
+        <div
+          class="card"
+          key={blog._id}
+          style={{
+            marginTop: "2%",
+            background: (0, 151, 19, 0.6),
+            color: "white"
+          }}
+          onClick={() => this.setDisplayBlog(blog)}
+        >
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{blog.title}</h6>
+            <h5 class="card-title">{blog.title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              {moment(blog.time).format("lll")}
+            </h6>
           </div>
         </div>
       );
@@ -32,58 +51,93 @@ class AllBlogs extends React.Component {
   };
   render() {
     return (
-      <React.Fragment>
+      <div style={{ maxHeight: "100%" }}>
         {this.state.blogs && this.state.displayBlog ? (
           <div
             style={{
               display: "flex",
               flexDirection: "row",
-              justifyContent: "space-between",
-              flexWrap: "wrap",
-              height: "100%"
+              justifyContent: "space-around",
+              flexWrap: "wrap"
             }}
           >
-            <div
-              className="bigBox col-lg-7 col-sm-12"
-              style={{ border: "solid 1px white" }}
-            >
+            <div className="bigBox col-lg-7 col-sm-12">
               <div
-                class="card text-center"
-                style={{ background: (0, 151, 19, 0.9), color: "white" }}
+                class="card  text-center"
+                style={{
+                  background: (0, 151, 19, 0.9),
+                  color: "white",
+                  border: "solid 1px white"
+                }}
               >
-                <div class="card-header">Currently Viewing</div>
-                <div class="card-body">
-                  <h5 class="card-title">
-                    {this.state.displayBlog.title.toUpperCase()}
-                  </h5>
-                  <p class="card-text">{this.state.displayBlog.body}</p>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column"
+                  }}
+                >
+                  <div class="card-header" style={{ alignSelf: "flex-start" }}>
+                    <u>
+                      {`Currently viewing: "${this.state.displayBlog.title.toUpperCase()}"`}
+                    </u>
+                  </div>
                   <div
+                    class="card-header"
                     style={{
+                      width: "100%",
                       display: "flex",
                       flexDirection: "row",
-                      justifyContent: "space-between",
                       flexWrap: "wrap"
                     }}
                   >
-                    <button className="form-control btn-success col-lg-5 col-sm-12">
-                      Reply
-                    </button>
-                    <button className="form-control btn-info col-lg-5 col-sm-12">
-                      Like
-                    </button>
+                    <span style={{ alignSelf: "flex-start" }}>
+                      <u>{`Likes:  ${this.state.displayBlog.likes || 0}`}</u>
+                    </span>
+                    <span style={{ marginLeft: "2%" }}>
+                      <u>
+                        {`Replies:  ${this.state.displayBlog.replies || 0}`}
+                      </u>
+                    </span>
                   </div>
                 </div>
-                <div class="card-footer text-muted">
-                  {moment(this.state.displayBlog.time).format("lll")}
+                <div
+                  class="card-body"
+                  style={{ maxHeight: "50vh", overflow: "auto" }}
+                >
+                  <p class="card-text">{this.state.displayBlog.body}</p>
+                </div>
+                <div
+                  style={{
+                    marginTop: "3%",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                    flexWrap: "wrap"
+                  }}
+                >
+                  <button className="form-control btn-success col-lg-3 col-sm-12">
+                    Reply
+                  </button>
+                  <div className="col-lg-3 col-sm-12 card-footer text-muted">
+                    {moment(this.state.displayBlog.time).format("lll")}
+                  </div>
+                  <button className="form-control btn-info col-lg-3 col-sm-12">
+                    Like
+                  </button>
                 </div>
               </div>
             </div>
-            <div style={{ display: "flex", flexDirection: "column" }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column"
+              }}
+            >
               {this.displayBlogs()}
             </div>
           </div>
         ) : null}
-      </React.Fragment>
+      </div>
     );
   }
 }
